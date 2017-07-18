d24301ce1242c3654826c781657074cf80858434
tiffine-koch <tiffine.koch@gmail.com>
2017-07-17 18:06:48 -0700

    Moves all routes into index.js

--- routes/sports.js
+++ routes/sports.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET sports page. */
-
-router.get('/sports', function(req, res, next) {
-  res.render('sports');
-});
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET sports page. */
+//
+// router.get('/sports', function(req, res, next) {
+//   res.render('sports');
+// });
